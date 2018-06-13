21fa0d9fad3e92e7baa5fc5b1a44c6a20333af25
Jordan Harris <jharris@wework.com>
2018-06-13 19:38:48 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until(looping_string)
-          levitation_force = 6
-          until levitation_force == 10
-          puts looping_string
-          levitation_force += 1
+      levitation_force = 6
+      until levitation_force == 10
+      puts looping_string
+      levitation_force += 1
   end
 end
