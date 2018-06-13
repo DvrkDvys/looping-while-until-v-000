1ed9040e71069db525ddb26df890ee9e48c9edb1
Jordan Harris <jharris@wework.com>
2018-06-13 19:42:01 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,7 @@ def using_while
           levitation_force = 6
           while levitation_force >= 6
           levitation_force += 1
-          puts "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+          puts "Wingardium Leviosa"
           if levitation_force >= 10 
       break
     end
