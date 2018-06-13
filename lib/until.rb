cdf24550a8e6dd3544eede369e46bfdced4f765f
Jordan Harris <jharris@wework.com>
2018-06-13 19:32:12 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -3,13 +3,15 @@ def using_until(looping_string)
         while levitation_force >= 6
         levitation_force += 1
         puts looping_string
-        
-end
-
-
-describe "#using_until" do
-	it "puts out the phrase 'Wingardium Leviosa' until the levitation force is incremented to 10" do 
-	  looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-	  expect{ using_until }.to output(looping_string).to_stdout
+        if levitation_force >= 10 
+    break
   end
 end
+
+
+# describe "#using_until" do
+# 	it "puts out the phrase 'Wingardium Leviosa' until the levitation force is incremented to 10" do 
+# 	  looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+# 	  expect{ using_until }.to output(looping_string).to_stdout
+#   end
+# end
